82b8349d9f458972d4a8eb6f0a4e665de7ef5f44
Lucas Sgalla <lucasgalla10@gmail.com>
2025-09-02 17:01:28 -0300

    Projeto criado

---
+++ src/main/java/sgallalucas/fitlab/FitlabApplication.java
@@ -0,0 +1,13 @@
+package sgallalucas.fitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sgallalucas/fitlab/FitlabApplicationTests.java
@@ -0,0 +1,13 @@
+package sgallalucas.fitlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
