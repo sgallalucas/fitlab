36ef231ff624a7f441b6d19fe5b92186f3c01630
Lucas Sgalla <lucasgalla10@gmail.com>
2025-09-08 20:47:06 -0300

    Adicionado o método MethodArgumentNotValidException

--- src/main/java/sgallalucas/fitlab/handler/GlobalExceptionHandler.java
+++ src/main/java/sgallalucas/fitlab/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package sgallalucas.fitlab.handler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sgallalucas.fitlab.dtos.errors.ErrorResponseDetails;
@@ -15,8 +16,25 @@ import java.util.List;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDetails> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        List<FieldError> fieldErrors = e.getFieldErrors();
+
+        List<FieldErrorDetails> fieldErrorDetails = fieldErrors.stream().map(fe -> new FieldErrorDetails(
+                fe.getField(), fe.getDefaultMessage())).toList();
+
+        ErrorResponseDetails details = new ErrorResponseDetails(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Validation error",
+                fieldErrorDetails
+        );
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(details);
+    }
+
     @ExceptionHandler(DuplicateRecordException.class)
     public ResponseEntity<ErrorResponseDetails> handleDuplicateRecordException(DuplicateRecordException e) {
+
         ErrorResponseDetails details = new ErrorResponseDetails(
                 HttpStatus.CONFLICT.value(),
                 "Validation error",
@@ -27,6 +45,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotAllowedOperationException.class)
     public ResponseEntity<ErrorResponseDetails> handleNotAllowedOperationException(NotAllowedOperationException e) {
+
         ErrorResponseDetails details = new ErrorResponseDetails(
                 HttpStatus.BAD_REQUEST.value(),
                 "Not allowed",
